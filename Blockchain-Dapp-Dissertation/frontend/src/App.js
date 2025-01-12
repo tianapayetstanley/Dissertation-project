a5cd9c4ef9757a8773dc7e802ba055f25d997ecf
Tiana Stanley <tianapayetstanley@gmail.com>
2025-01-12 18:04:37 +0000

    create web3 react app

---
+++ Blockchain-Dapp-Dissertation/frontend/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import './App.css'
+
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    //fetch account 0x86Af90deC474618DFcB911e2617B74773c3b1b39
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { account: '' }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello, World!</h1>
+        <p>Your account: {this.state.account}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
