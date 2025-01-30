8888d66d075742d9c01bea09a32f894a70c5d457
Tiana Stanley <tianapayetstanley@gmail.com>
2025-01-30 21:00:59 +0000

    num guessing game

---
+++ projects/guessing_game/target/debug/build/proc-macro2-7b27d5a0c1fe56a9/build_script_build-7b27d5a0c1fe56a9.d
@@ -0,0 +1,5 @@
+/home/tiana/projects/guessing_game/target/debug/build/proc-macro2-7b27d5a0c1fe56a9/build_script_build-7b27d5a0c1fe56a9: /home/tiana/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs
+
+/home/tiana/projects/guessing_game/target/debug/build/proc-macro2-7b27d5a0c1fe56a9/build_script_build-7b27d5a0c1fe56a9.d: /home/tiana/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs
+
+/home/tiana/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs:
---
+++ projects/guessing_game/target/debug/deps/guessing_game-46bb552ecb62f83c.d
@@ -0,0 +1,5 @@
+/home/tiana/projects/guessing_game/target/debug/deps/guessing_game-46bb552ecb62f83c: src/main.rs
+
+/home/tiana/projects/guessing_game/target/debug/deps/guessing_game-46bb552ecb62f83c.d: src/main.rs
+
+src/main.rs:
